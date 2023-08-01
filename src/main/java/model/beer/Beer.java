6688b28b6b3086ca37c4d0027990c18791d03f8b
jangheepark <jeanparkmk@gmail.com>
2023-08-02 03:42:27 +0900

    feat: Beer, OutBeer, Brewery, Style의 model 작성

--- src/main/java/model/beer/Beer.java
+++ src/main/java/model/beer/Beer.java
@@ -1,4 +1,19 @@
 package model.beer;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+@ToString
+@Builder
+@AllArgsConstructor
+@Getter
 public class Beer {
+    private Integer id; // Primary Key
+    private Integer style_id;
+    private String name;
+    private Timestamp createdAt;
+
 }
--- src/main/java/model/style/Style.java
+++ src/main/java/model/style/Style.java
@@ -1,4 +1,24 @@
 package model.style;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Getter
+@ToString
 public class Style {
+    private Integer id; // Primary Key
+    private Integer breweryId ; // Style(1) : Brewery(1)
+    private String name;
+    private Timestamp createdAt;
+
+    @Builder
+    public Style(int id, int breweryId, String name, Timestamp createdAt){
+        this.id = id;
+        this.breweryId = breweryId;
+        this.name = name;
+        this.createdAt = createdAt;
+    }
 }
