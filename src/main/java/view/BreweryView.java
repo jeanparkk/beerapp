11351bff52b39d22458ab371eed48cdb37986faa
jangheepark <jeanparkmk@gmail.com>
2023-08-10 04:41:31 +0900

    feat : Brewery, Style View 구현 BreweryView.java 코드 추가 StyleView.java 코드 추가

--- src/main/java/view/BreweryView.java
+++ src/main/java/view/BreweryView.java
@@ -1,7 +1,11 @@
 package view;
 
+import model.brewery.Brewery;
 import service.BreweryService;
 
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Map;
 
 public class BreweryView {
@@ -11,10 +15,51 @@ public class BreweryView {
     }
 
     public void create(Map<String, Object> params) {
-
+        try {
+            String name = params.get("name").toString();
+            System.out.println(breweryService.createBrewery(name));
+        } catch (Exception e) {
+            throw new RuntimeException("잘못된 요청입니다.");
+        }
     }
 
     public void findBreweries() {
+        try {
+            List<Brewery> breweries = breweryService.getAllBreweries();
+            String[] tableHead = {"양조장 ID", "양조장 이름", "등록일"};
+            printRow(tableHead);
+            for (Brewery brewery : breweries) {
+                String[] tableData = {
+                        brewery.getId().toString(),
+                        brewery.getName(),
+                        getFormatDate(brewery.getCreatedAt())
+                };
+                printRow(tableData);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("잘못된 요청입니다.");
+        }
 
+    }
+    private void printRow(String[] input) {
+        StringBuilder builder = new StringBuilder();
+        for (String str : input) {
+            builder.append(formatData(str));
+        }
+        System.out.println(builder);
+    }
+
+    private String formatData(String data) {
+        if (data == null) {
+            return "";
+        }
+        return String.format("%-8s", data);
+    }
+    private String getFormatDate(Timestamp timestamp) {
+        if (timestamp == null) {
+            return "";
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY.MM.dd");
+        return timestamp.toLocalDateTime().format(formatter);
     }
 }
--- src/main/java/view/StyleView.java
+++ src/main/java/view/StyleView.java
@@ -1,7 +1,11 @@
 package view;
 
+import dto.StyleRespDto;
 import service.StyleService;
 
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Map;
 
 public class StyleView {
@@ -11,10 +15,56 @@ public class StyleView {
     }
 
     public void create(Map<String, Object> params) {
+        try {
+            int breweryId = Integer.parseInt(params.get("breweryId").toString());
+            String name = params.get("name").toString();
+            System.out.println(styleService.createStyle(breweryId, name));
+        } catch (Exception e) {
+            throw new RuntimeException("잘못된 요청입니다.");
+        }
 
     }
 
     public void findStyles() {
+        try {
+            List<StyleRespDto> styles = styleService.getAllStyles();
+            String[] tableHead = {"스타일 ID", "스타일명", "양조장 ID", "양조장명"};
+            printRow(tableHead);
+            for (StyleRespDto style : styles) {
+                String[] tableData = {
+                        style.getId().toString(),
+                        style.getStyleName(),
+                        style.getBreweryId().toString(),
+                        style.getBreweryName()
+                };
+                printRow(tableData);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("잘못된 요청입니다.");
+        }
 
     }
+
+    private void printRow(String[] input) {
+        StringBuilder builder = new StringBuilder();
+        for (String str : input) {
+            builder.append(formatData(str));
+        }
+        System.out.println(builder);
+    }
+
+    private String formatData(String data) {
+        if (data == null) {
+            return "";
+        }
+        return String.format("%-8s", data);
+    }
+    private String getFormatDate(Timestamp timestamp) {
+        if (timestamp == null) {
+            return "";
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY.MM.dd");
+        return timestamp.toLocalDateTime().format(formatter);
+    }
+
 }
